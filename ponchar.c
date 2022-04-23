0e6c138890053fb90513205f392fa4bdd5c4facc
Alberto Marrugo <ajmarrugos@gmail.com>
2022-04-22 17:33:24 -0700

    Added function to put chars in stdout

---
+++ ponchar.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+
+/**
+ * ponchar - Will write the char c to stdout
+ * @c: The character to print
+ * Return: On success 1. On error, -1 is returned
+ */
+int ponchar(char c)
+{
+	return (write(1, &c, 1));
+}
--- shell.h
+++ shell.h
@@ -12,6 +12,9 @@
 /* Removes new line */
 void nnline(char *in);
 
+/* Writes a char in stdout */
+int ponchar(char c);
+
 /* Prints a string */
 void str_printer(char *s);
 
