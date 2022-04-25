9aa99bb49c11282111e9bdfc8a3e61b52e35a05d
Vozudo <jploperatobon@gmail.com>
2022-04-25 00:04:28 -0500

    exit built in implemented

---
+++ exit.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+
+/**
+ * _exit - will exit the shell en type the comand exit
+ * @in: Buffer
+ * @pasedin: the arguments
+ * @_exitst: exit status
+ */
+
+void _exit(char *in, char **pasedin, int *_exitst)
+{
+    if (_strcmp(pasedin[0], "exit") == 0)
+	{
+		free(pasedin);
+		free(in);
+		exit(*_exitst);
+}
